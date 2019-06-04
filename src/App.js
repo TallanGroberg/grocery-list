67d6692ba05c071182a816b88a08b1691ff23457
tallan groberg <tallan.taven@gmail.com>
2019-06-04 08:00:29 -0600

    working to do list

--- src/App.js
+++ src/App.js
@@ -1,31 +1,60 @@
 import React, { Component } from 'react';
-import './App.css';
 import List from './List'
+import TodoForm from './TodoForm'
+import './App.css';
 
 class App extends Component {
-  state = {
-    todos: [
-      {id: 1, name: "pickles", complete: true, },
-      {id: 2, name: "kumbucha", complete: true, },
-      {id: 3, name: "hemp lip balm", complete: true, }
-    ]
+  constructor(props) {
+    super(props);
+
+    this.state = {
+     todos: [
+       { id: 1, name: "spinach", complete: false, },
+       { id: 2, name: "mushrooms", complete: false, },
+       { id: 3, name: "kumbucha", complete: false, },
+     ]
+   };
+
+   this.addItem = this.addItem.bind(this);
   }
 
-  renderTodos = () => {
-    const { todos, } = this.state;
-    return todos.map( todo => <li key={todo.id}> {todo.name}</li>
-    )
-  };
+
+  getUniqueId = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+  }
+
+addItem = (name) => {
+  const todo = { name, id: this.getUniqueId(), complete: false, };
+  this.setState({ todos: [todo, ...this.state.todos], });
+};
+
+handleClick = (id) => {
+  this.setState({
+    todos: this.state.todos.map( todo => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          complete: !todo.complete,
+        }
+      }
+      return todo;
+    })
+  })
+}
+
+
+// old js
 
 
   render() {
     return (
       <div>
-        <ul>
-          { this.renderTodos() }
-        </ul>
-      </div>
-    )
+      <TodoForm  addItem={this.addItem} />
+      <List name="Grocery list" items={this.state.todos} todoClick={this.handleClick} />
+    </div>
+    );
   }
 }
 
--- src/List.js
+++ src/List.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
+import Todo from "./Todo";
 
 const List = (props) => (
   <div>
-  <h1>{props.name}</h1>
-  <ul>
-    {props.item.map( item => <li key={item.id}>item.name</li>) }
-      </ul>
-    </div>
+    <h1>{props.name}</h1>
+    <ul>
+      { props.items.map( item => <Todo key={item.id} {...item} todoClick={props.todoClick} />) }
+    </ul>
+  </div>
 );
+
 
 export default List;
